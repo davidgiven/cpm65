208a6da01726813a6730755e7a1a1e9badeb897d
Ivo van Poorten <ivopvp@gmail.com>
2023-09-01 00:06:07 +0200

    Add 3x6 font I drew in Envision. Including padding, all characters are 4x6 and replicated on the left and right side of the corresponding character. Index 0-95 maps to 32-127 ASCII. Each character is 6 bytes long.

---
+++ third_party/fonts/atari/conv3x6.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+in=ivo3x6.fnt
+out=ivo3x6.dat
+outinc=ivo3x6.inc
+
+> $out
+
+for i in `seq 0 95` ; do
+    dd if=$in of=$out bs=1 skip=$(($i*8)) count=6 conv=notrunc seek=$(($i*6)) \
+        status=none
+done
+
+xxd -c 6 -g 1 -i ivo3x6.dat | head -n -2 | grep -v unsigned | \
+    sed 's/,$//g; s/^/    .byte/g; s/0x/$/g' > $outinc
