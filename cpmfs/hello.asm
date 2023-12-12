a7f52116066948124054ec9bbe6736530a24c093
David Given <dg@cowlark.com>
2023-12-12 20:22:25 +0100

    Fix formatting.

--- cpmfs/hello.asm
+++ cpmfs/hello.asm
@@ -1,12 +1,13 @@
 start:
-	lda #<message
-	ldx #>message
-	ldy #9
-	jsr start - 3
-	rts
+    lda #<message
+    ldx #>message
+    ldy #9
+    jsr start - 3
+    rts
 
 message:
-	.byte "Hello, world!", 13, 10, 0
+    .byte "Hello, world!", 13, 10, 0
 
+\ vim: ts=4 sw=4 et
 
 
