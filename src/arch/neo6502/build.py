220c079a30edf968fe2e55cd5f049a5b0279d0a5
David Given <dg@cowlark.com>
2024-02-07 23:20:52 +0100

    Archival non-functioning checkin.

--- src/arch/neo6502/build.py
+++ src/arch/neo6502/build.py
@@ -4,54 +4,49 @@ from build.llvm import llvmrawprogram, llvmcfile
 llvmcfile(
     name="bios_obj",
     srcs=["./neo6502.S"],
-    deps=["include", "src/lib+bioslib"],
+    deps=["include", "src/lib+bioslib", "src/bdos+bdoslib"],
 )
 
-#llvmrawprogram(
-#    name="bios_prelink",
-#    srcs=[".+bios_obj"],
-#    deps=["src/lib+bioslib"],
-#    linkscript="./neo6502-prelink.ld",
-#    ldflags=["--defsym=BIOS_SIZE=0x4000"],
-#)
+llvmrawprogram(
+    name="bios_prelink",
+    srcs=[".+bios_obj"],
+    deps=["src/lib+bioslib", "src/bdos+bdoslib"],
+    linkscript="./neo6502-prelink.ld",
+    ldflags=["--defsym=BIOS_SIZE=0x4000"],
+)
 
 llvmrawprogram(
     name="bios",
     srcs=[".+bios_obj"],
-    deps=["src/lib+bioslib"],
+    deps=[
+        ".+bios_prelink",
+        "scripts/size.awk",
+        "src/lib+bioslib",
+        "src/bdos+bdoslib",
+    ],
     linkscript="./neo6502.ld",
+    ldflags=[
+        "--defsym=BIOS_SIZE=$$($(LLVM)/llvm-objdump --section-headers {deps[0]} "
+        + "| gawk --non-decimal-data -f scripts/size.awk)"
+    ],
 )
-#llvmrawprogram(
-#    name="bios",
-#    srcs=[".+bios_obj"],
-#    deps=[
-#        ".+bios_prelink",
-#        "scripts/size.awk",
-#        "src/lib+bioslib",
-#    ],
-#    linkscript="./apple2e.ld",
-#    ldflags=[
-#        "--defsym=BIOS_SIZE=$$($(LLVM)/llvm-objdump --section-headers {deps[0]} "
-#        + "| gawk --non-decimal-data -f scripts/size.awk)"
-#    ],
-#)
 
-#mkcpmfs(
+# mkcpmfs(
 #    name="diskimage",
 #    format="appleiie",
 #    bootimage=".+bios_shuffled",
 #    size=143360,
-#    items={"0:ccp.sys@sr": "src+ccp", "0:bdos.sys@sr": "src+bdos"}
+#    items={"0:ccp.sys@sr": "src+ccp", "0:bdos.sys@sr": "src/bdos"}
 #    | MINIMAL_APPS
 #    | MINIMAL_APPS_SRCS
 #    | BIG_APPS
 #    | BIG_APPS_SRCS,
-#)
+# )
 #
-#mametest(
+# mametest(
 #    name="mametest",
 #    target="apple2e",
 #    diskimage=".+diskimage",
 #    imagetype=".po",
 #    script="./mame-test.lua",
-#)
+# )
