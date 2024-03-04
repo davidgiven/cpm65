b608a42ae0fe71d8b5c070b61e14bdb2754ab9c4
David Given <dg@cowlark.com>
2024-03-04 20:50:23 +0100

    Hopefully fix the MAME tests.

--- scripts/oric-mame-test.sh
+++ scripts/oric-mame-test.sh
@@ -4,4 +4,11 @@ set -e
 diskimage=/tmp/$$.$4
 trap "rm -f $diskimage" EXIT
 cp $2 $diskimage
-SDL_VIDEODRIVER=dummy SDL_AUDIODRIVER=dummy chronic mame $1 -ext microdisc -flop $diskimage -video none -autoboot_script $3 -nothrottle -nosleep -snapshot_directory snap -sound none -rompath roms
+SDL_VIDEODRIVER=dummy SDL_AUDIODRIVER=dummy chronic mame $1 -ext microdisc -flop $diskimage -video none -autoboot_script $3 -nothrottle -nosleep -snapshot_directory snap -sound none -rompath roms || \
+    ( \
+        echo "Failed!" && \
+        echo "Run this to debug:" && \
+        echo "mame $1 -ext microdisc -flop $2 -autoboot_script $3 -nothrottle -nosleep -snapshot_directory snap -rompath roms" &&
+        false \
+    )
+
