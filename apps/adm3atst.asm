947a57f5528adf85274f44390cab70cdc552b962
Ivo van Poorten <ivopvp@gmail.com>
2024-05-03 03:49:10 +0200

    Ignore all unknown control characters. Remove ZP cruft from when we used the jump table dispatcher.

--- apps/adm3atst.asm
+++ apps/adm3atst.asm
@@ -1,12 +1,23 @@
 .include "cpm65.inc"
 
 .label test_string
+.label BIOS
 
 .zproc start
     lda #<test_string
     ldx #>test_string
     ldy #BDOS_PRINTSTRING
     jsr BDOS
+
+    ldy #BDOS_GET_BIOS
+    jsr BDOS
+    sta BIOS+1
+    stx BIOS+2
+
+    lda #0
+    ldy #BIOS_CONOUT
+BIOS:
+    jsr $1234
 
     rts
 .zendproc
@@ -21,5 +32,6 @@ test_string:
     .byte 27, '=', 32+10, 32+10, "Position 10,10"
     .byte 27, '=', 32+15, 32+20, "Move to 20,15"
     .byte 27, '=', 32+20, 32+5, 27, "IIgnore invalid escape", 13,10
+    .byte "Ignore unknown control characters",13,10, 1,2,3,4,5,6
     .byte 27, '=', 32+22, 32+2, "Press ENTER to scroll", 13,10
     .byte 0
