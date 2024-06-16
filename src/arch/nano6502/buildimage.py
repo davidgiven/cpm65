afae0733ec4d8ed7a8b560d433572058a09c14d9
Henrik L <henrik.lofgren@gmail.com>
2024-06-16 15:39:51 +0200

    Changed to 16 1MB drives

--- src/arch/nano6502/buildimage.py
+++ src/arch/nano6502/buildimage.py
@@ -5,23 +5,26 @@ import os
 
 bdos_offset = 512*256
 cpmfs_offset = 512*256
-cpmfs_size = 1024*4096
+cpmfs_size = 1024*1024
 
 bios_filename = '.obj/src/arch/nano6502/+nano6502/+nano6502'
 bdos_filename = '.obj/src/bdos/+bdos/+bdos'
 cpmfs_filename = '.obj/src/arch/nano6502/+cpmfs/src/arch/nano6502/+cpmfs.img'
 cpmfs_empty_filename = '.obj/src/arch/nano6502/+emptycpmfs/src/arch/nano6502/+emptycpmfs.img'
 output_filename = './nano6502.img'
+output_sysonly_filename = './nano6502_sysonly.img'
 
 size = os.path.getsize(bios_filename)
 
 infile=open(bios_filename, "rb")
 outfile=open(output_filename, "wb")
+sysfile=open(output_sysonly_filename, "wb")
 
 byte=infile.read(1)
 # Write BIOS
 while byte:
     outfile.write(byte)
+    sysfile.write(byte)
     byte=infile.read(1)
 
 padding = bdos_offset - size;
@@ -29,6 +32,7 @@ out = 0
 
 while padding:
     outfile.write(out.to_bytes(1,"little"))
+    sysfile.write(out.to_bytes(1,"little"))
     padding = padding - 1;
 
 infile.close()
@@ -41,6 +45,7 @@ infile = open(bdos_filename, "rb")
 byte=infile.read(1)
 while byte:
     outfile.write(byte)
+    sysfile.write(byte)
     byte=infile.read(1)
 
 padding = cpmfs_offset - size;
@@ -48,6 +53,7 @@ out = 0
 
 while padding:
     outfile.write(out.to_bytes(1,"little"))
+    sysfile.write(out.to_bytes(1,"little"))
     padding = padding - 1;
 
 infile.close()
@@ -60,66 +66,36 @@ infile = open(cpmfs_filename, "rb")
 byte=infile.read(1)
 while byte:
     outfile.write(byte)
+    sysfile.write(byte)
     byte=infile.read(1)
 
 padding = cpmfs_size - size;
 out=0
 while padding:
     outfile.write(out.to_bytes(1,"little"))
+    sysfile.write(out.to_bytes(1,"little"))
     padding = padding - 1;
 
 infile.close()
+sysfile.close()
 
-# Write empty drive B
-size = os.path.getsize(cpmfs_empty_filename)
+# Write empty drives B-O
+for i in range(15):
+    size = os.path.getsize(cpmfs_empty_filename)
 
-infile = open(cpmfs_empty_filename, "rb")
+    infile = open(cpmfs_empty_filename, "rb")
 
-byte=infile.read(1)
-while byte:
-    outfile.write(byte)
     byte=infile.read(1)
+    while byte:
+        outfile.write(byte)
+        byte=infile.read(1)
 
-padding = cpmfs_size - size;
-out=0
-while padding:
-    outfile.write(out.to_bytes(1,"little"))
-    padding = padding - 1;
+    padding = cpmfs_size - size;
+    out=0
+    while padding:
+        outfile.write(out.to_bytes(1,"little"))
+        padding = padding - 1;
 
-infile.close()
+    infile.close()
 
-# Write empty drive C
-size = os.path.getsize(cpmfs_empty_filename)
-
-infile = open(cpmfs_empty_filename, "rb")
-
-byte=infile.read(1)
-while byte:
-    outfile.write(byte)
-    byte=infile.read(1)
-
-padding = cpmfs_size - size;
-out=0
-while padding:
-    outfile.write(out.to_bytes(1,"little"))
-    padding = padding - 1;
-infile.close()
-
-# Write empty drive D
-size = os.path.getsize(cpmfs_empty_filename)
-
-infile = open(cpmfs_empty_filename, "rb")
-
-byte=infile.read(1)
-while byte:
-    outfile.write(byte)
-    byte=infile.read(1)
-
-padding = cpmfs_size - size;
-out=0
-while padding:
-    outfile.write(out.to_bytes(1,"little"))
-    padding = padding - 1;
-
-infile.close()
 outfile.close()
