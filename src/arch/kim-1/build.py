f5caa36e114d37ba771d279270590a3e03fa3eef
Eduardo Casino <mail@eduardocasino.es>
2024-06-23 23:49:46 +0200

    Tons of fixes, modularize build, first, non-working port for sdcard. K-1013 port working for single disk

--- src/arch/kim-1/build.py
+++ src/arch/kim-1/build.py
@@ -1,6 +1,6 @@
-from build.ab import normalrule
 from tools.build import mkimd, mkcpmfs
-from build.llvm import llvmrawprogram
+from build.llvm import llvmrawprogram, llvmclibrary
+from build.zip import zip
 from config import (
     MINIMAL_APPS,
     MINIMAL_APPS_SRCS,
@@ -9,18 +9,47 @@ from config import (
     PASCAL_APPS,
 )
 
+llvmclibrary(
+    name="libsd", srcs=["./libsd.S"], cflags=["-I ."], deps=["include"]
+)
+
+llvmclibrary(
+    name="k-1013", srcs=["./k-1013.S"], cflags=["-I ."], deps=["include"]
+)
+
 llvmrawprogram(
-    name="bios",
-    srcs=["./kim-1.S"],
-    deps=["include", "src/lib+bioslib"],
-    linkscript="./kim-1.ld",
+    name="bios-k1013",
+    srcs=["./kim-1-k1013.S"],
+    deps=["./kim-1.S", "./kim-1.inc", "include", "src/lib+bioslib", ".+k-1013"],
+    linkscript="./kim-1-k1013.ld",
+)
+
+llvmrawprogram(
+    name="bios-sdcard",
+    srcs=["./kim-1-sdcard.S"],
+    deps=["./kim-1.S", "./kim-1.inc", "include", "src/lib+bioslib", ".+libsd"],
+    linkscript="./kim-1-sdcard.ld",
 )
 
 mkcpmfs(
-    name="rawdiskimage",
+    name="rawdiskimage-k1013",
     format="k-1013",
-    bootimage=".+bios",
+    bootimage=".+bios-k1013",
     size=256 * 77 * 26,
+    items={"0:ccp.sys@sr": "src+ccp", "0:bdos.sys@sr": "src/bdos"}
+    | {"0:pasc.pas": "third_party/pascal-m+pasc_pas_cpm"}
+    | MINIMAL_APPS
+    | MINIMAL_APPS_SRCS
+    | BIG_APPS
+    | BIG_APPS_SRCS
+    | PASCAL_APPS,
+)
+
+mkcpmfs(
+    name="rawdiskimage-sdcard",
+    format="sdcard",
+    bootimage=".+bios-sdcard",
+    size=512 * 2048 * 16,
     items={"0:ccp.sys@sr": "src+ccp", "0:bdos.sys@sr": "src/bdos"}
     | MINIMAL_APPS
     | MINIMAL_APPS_SRCS
@@ -29,17 +58,20 @@ mkcpmfs(
     | PASCAL_APPS,
 )
 
-mkimd(name="diskimage", src=".+rawdiskimage")
+mkimd(name="diskimage-k1013", src=".+rawdiskimage-k1013")
 
-normalrule(
-    name="distro",
-    ins=[
-        ".+diskimage",
-        "src/arch/kim-1/boot+cpm65.bin",
-    ],
-    outs=["kim-1.zip"],
-    commands=[
-        "zip -9qj {outs[0]} {ins}",
-    ],
-    label="ZIP",
+zip(
+    name="distro-k1013",
+    items={
+        "diskimage.imd": ".+diskimage-k1013",
+        "boot.bin": "src/arch/kim-1/boot+boot.bin",
+    },
+)
+
+zip(
+    name="distro-sdcard",
+    items={
+        "diskimage.raw": ".+rawdiskimage-sdcard",
+        "bootsd.bin": "src/arch/kim-1/boot+bootsd.bin",
+    },
 )
