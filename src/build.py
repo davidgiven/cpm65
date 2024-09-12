34b6148e0b6f76141062936298ee3669e33dbc8c
David Given <dg@cowlark.com>
2024-09-13 01:37:11 +0200

    Do a tiny build variant of the CCP. Make the Apple II port use it, as it seems to be running out of memory.

--- src/build.py
+++ src/build.py
@@ -3,3 +3,10 @@ from build.llvm import llvmprogram
 llvmprogram(
     name="ccp", srcs=["./ccp.S"], deps=["include", "lib+bdos", "lib+xfcb"]
 )
+
+llvmprogram(
+    name="ccp-tiny",
+    srcs=["./ccp.S"],
+    deps=["include", "lib+bdos", "lib+xfcb"],
+    cflags=["-DTINY"],
+)
