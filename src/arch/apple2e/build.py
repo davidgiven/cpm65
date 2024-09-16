e6d377d274e6b8e09a2c42cdb395038963c475b7
David Given <dg@cowlark.com>
2024-09-16 22:54:28 +0200

    Finish off the Apple IIe SCREEN driver; fix lots of embarrassing bugs (turns out the screen is 24 lines, not 25...)

--- src/arch/apple2e/build.py
+++ src/arch/apple2e/build.py
@@ -54,11 +54,13 @@ mkcpmfs(
         "0:ccp.sys@sr": "src+ccp-tiny",
         "0:bdos.sys@sr": "src/bdos",
         "0:scrntest.com": "apps+scrntest",
+        "0:cls.com": "apps+cls",
+        "0:atbasic.com": "third_party/altirrabasic",
+        "0:atbasic.txt": "cpmfs+atbasic_txt_cpm",
+        "0:qe.com": "apps+qe",
     }
     | MINIMAL_APPS
-    | MINIMAL_APPS_SRCS
-    | BIG_APPS
-    | BIG_APPS_SRCS,
+    | MINIMAL_APPS_SRCS,
 )
 
 mametest(
