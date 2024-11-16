05a68f70e92465cd2fb7399aa840e3d685a905d7
Ivo van Poorten <ivopvp@gmail.com>
2024-11-16 21:00:24 +0100

    osi: Add tty630/screen630 driver for the Ohio Scientific Model 630 graphics card.

--- src/arch/osi/utils/build.py
+++ src/arch/osi/utils/build.py
@@ -2,3 +2,4 @@ from build.llvm import llvmprogram
 
 llvmprogram( name="tty540b", srcs=["./tty540b.S"], deps=[ "include", ],)
 llvmprogram( name="scrvt100", srcs=["./scrvt100.S"], deps=[ "include", ],)
+llvmprogram( name="tty630", srcs=["./tty540b.S"], deps=[ "include", ], cflags=["-DOSI630"],)
