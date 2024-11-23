4c268b917f4a9f2b7c4811db0116a16f6754ffbe
Andreas Baumann <mail@andreasbaumann.cc>
2024-11-23 13:59:09 +0100

    removed local small CCP hack

--- src/build.py
+++ src/build.py
@@ -1,7 +1,6 @@
 from build.llvm import llvmprogram
 
 llvmprogram(
-#    name="ccp", srcs=["./ccp.S"], cflags=["-DCCP_MONITOR"], deps=["include", "lib+bdos", "lib+xfcb"]
     name="ccp", srcs=["./ccp.S"], deps=["include", "lib+bdos", "lib+xfcb"]
 )
 
