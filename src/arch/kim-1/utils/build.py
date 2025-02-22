a48a5d94c561071a32b379cdee40d63d84e42e07
Eduardo Casino <mail@eduardocasino.es>
2025-02-22 13:08:27 +0100

    Integrate FDC functions into program source

--- src/arch/kim-1/utils/build.py
+++ src/arch/kim-1/utils/build.py
@@ -1,8 +1,4 @@
-from build.llvm import llvmprogram,llvmclibrary
-
-llvmclibrary(
-    name="k-1013", srcs=["./k-1013.S"], cflags=["-I src/arch/kim-1"], deps=["include", "src/arch/kim-1/kim-1.inc", "src/arch/kim-1/k-1013.inc"]
-)
+from build.llvm import llvmprogram
 
 llvmprogram(
     name="format",
@@ -10,7 +6,6 @@ llvmprogram(
     cflags=["-I src/arch/kim-1"],
     deps=[
         "include",
-        ".+k-1013",
         "src/arch/kim-1/k-1013.inc",
     ],
 )
